a29ffc8fb625066b48f00e10b42c72ac4a0ffd00
Shena9y <hamoohamdy100@gmail.com>
2023-11-17 20:30:44 +0200

    T3 Done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Template-three
 template-three for practicing on html, css
+
+## Hi Welcome in the repo 
+
+## this is my practicing in the html,css from the Elzero Web school Tutorial
---
+++ js/main.js
@@ -0,0 +1,25 @@
+// The time we need to reach
+let countDown = new Date("Dec 31, 2023 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  let DateNow = new Date().getTime(); // the current Date
+
+  let dateDiff = countDown - DateNow; //get the difference between the two Dates
+
+  // get time units
+
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
